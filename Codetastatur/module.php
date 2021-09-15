80526738b827d8924209425837870fef49a824b9
DonCri <36209368+DonCri@users.noreply.github.com>
2021-09-15 11:15:56 +0200

    Update module.php

--- Codetastatur/module.php
+++ Codetastatur/module.php
@@ -97,8 +97,8 @@ class MaxFlexCodepanel extends IPSModule {
 								SetValue($this->GetIDForIdent("CODE"), 0);
 								SetValue($this->GetIDForIdent("CODEOK"), false);
 								//$this->SetLED(0);
-								$this->SetLED(1);
-								//$this->SetLED(10);
+								//$this->SetLED(1);
+								$this->SetLED(2);
 							} else{
 								$typedCode .= 3;
 								SetValue($this->GetIDForIdent("CODE"), $typedCode);
